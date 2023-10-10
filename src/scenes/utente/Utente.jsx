af041624291378d7e85433589fa20395fec13e0f
Michele Lucini <mic.lucini@gmail.com>
2023-10-10 22:00:10 +0200

    Fix pagina utente

--- src/scenes/utente/Utente.jsx
+++ src/scenes/utente/Utente.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { useNavigator } from "@contexts/NavigatorContext";
 import * as apiPublic from "@api/public";
-import { getConstantDescriptionByValue, ACCOUNT_TYPE } from "@logic/constants";
+import React, { useState, useCallback, useEffect } from "react";
 import TextInput from '@components/textInput';
+import { useNavigator } from "@contexts/NavigatorContext";
 
 const Utente = () => {
   const { currentRoute } = useNavigator();
+  
   const [utente, setUtente] = useState(null);
 
   const loadUtente = useCallback(async () => {
-    const data = await apiPublic.GetUser({ id: currentRoute.params[0] });
+    const data = await apiPublic.GetUser({ userId: currentRoute.params[0] });
     setUtente(data);
   }, [currentRoute])
 
@@ -23,131 +23,32 @@ const Utente = () => {
 
   return (
     <>
-      <br></br>
-      <div className='row'>
-        <div className='col'>
-          <span>Utente</span>
-        </div>
+    <br></br>
+    <div className='row'>
+      <div className='col'>
+        <span className='page-title'>Profilo utente di {utente.advertisementName}</span>
       </div>
-      <br></br>
-      <div className='row'>
-        <div className='col'>
-          <label>Informazioni speciali</label>
-        </div>
+    </div>
+    <div className='row'>
+      <div className='col'>
+        <TextInput label="Username" value={utente.advertisementName} disabled />
       </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="id" value={utente.id} disabled />
-        </div>
+    </div>
+    <div className='row'>
+      <div className='col'>
+        <TextInput label="Email" value={utente.email} disabled />
       </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="isAdmin" value={utente.isAdmin} disabled />
-        </div>
+    </div>
+    <div className='row'>
+      <div className='col'>
+        <TextInput label="Cellulare" value={utente.cel} disabled />
       </div>
-      <br></br>
-      <div className='row'>
-        <div className='col'>
-          <label>Informazioni di base</label>
-        </div>
+    </div>
+    <div className='row'>
+      <div className='col'>
+        <TextInput label="Telefono" value={utente.tel} disabled />
       </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="accountType" value={getConstantDescriptionByValue(ACCOUNT_TYPE, utente.accountType)} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="lastName" value={utente.lastName} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="name" value={utente.name} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="advertisementName" value={utente.advertisementName} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="businessName" value={utente.businessName} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="yearBirth" value={utente.yearBirth} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="codiceFiscale" value={utente.codiceFiscale} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="partitaIva" value={utente.partitaIva} disabled />
-        </div>
-      </div>
-      <br></br>
-      <div className='row'>
-        <div className='col'>
-          <label>Informazioni di indirizzo</label>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="country" value={utente.country} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="province" value={utente.province} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="zipCode" value={utente.zipCode} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="city" value={utente.city} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="street" value={utente.street} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="civic" value={utente.civic} disabled />
-        </div>
-      </div>
-      <br></br>
-      <div className='row'>
-        <div className='col'>
-          <label>Informazioni di contatto</label>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="cel" value={utente.cel} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="email" value={utente.email} disabled />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <TextInput label="tel" value={utente.tel} disabled />
-        </div>
-      </div>
+    </div>
     </>
   );
 };
